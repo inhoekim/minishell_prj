81516308a678949f388bed495d4dd31eef0d5e84
wwwlnyy <naylee@c4r10s7.42seoul.kr>
2023-08-09 16:45:21 +0900

    edit: tokenizer 관련 함수들 작업

--- tokenizer.h
+++ tokenizer.h
@@ -1,14 +1,43 @@
 #ifndef TOKNIZER_H
 # define TOKNIZER_H
 
-# include "minishell.h"
+#include <stdio.h>
 
-t_tokenizer	get_next_token(char *line);
-t_tokenizer	scan_char_token(char *start, t_tokenizer *token);
-t_tokenizer	scan_word_token(char *start, t_tokenizer *token);
-t_tokenizer	make_token(char *start, t_tokenizer *token, t_symbol type);
+typedef enum e_bool
+{
+	FALSE,
+	TRUE,
+}   t_bool;
 
-char	*skip_whitespace(char *c);
-t_bool	match(t_tokenizer *token, char matchword);
+typedef enum e_symbol
+{
+    WORD, //0
+    LESS, //1
+    GREAT, //2
+    DGREAT, //3
+    DLESS, //4
+    AND_IF, //5
+    OR_IF, //6
+    PIPE, //7
+	SUBSHELL, //8
+	E0F, //9
+}   t_symbol;
+
+typedef struct s_tokenizer
+{
+    t_symbol	type;
+	char		*start;
+	char		*end;
+} t_tokenizer;
+
+t_tokenizer	get_next_token(t_tokenizer *tokenizer);
+t_tokenizer	scan_char_token(t_tokenizer *tokenizer);
+t_tokenizer	make_token(t_tokenizer *tokenizer, t_symbol type);
+t_tokenizer	scan_word_token(t_tokenizer *tokenizer);
+
+void		reset_tokenizer(t_tokenizer *tokenizer);
+void		skip_whitespace(t_tokenizer *token);
+t_bool		match(t_tokenizer *tokenizer, char matchword);
+t_bool		string_close(t_tokenizer *tokenizer, char c);
 
 #endif
--- tokenizer_utils.c
+++ tokenizer_utils.c
@@ -1,36 +1,41 @@
 #include "tokenizer.h"
 
-// void	skip_whitespace(t_tokenizer *token)
-// {
-// 	while ((*token->current_token) == 32 || \
-// 		((*token->current_token) >= 9 && (*token->current_token) <= 13))
-// 		token->current_token++;
-// }
-
-char	*skip_whitespace(char *c)
+void	reset_tokenizer(t_tokenizer *tokenizer)
 {
-	while (*c == 32 || (*c >= 9 && *c <= 13))
-		c++;
-	return (c);
+	if (tokenizer->type == SUBSHELL)
+		tokenizer->end++;
+	if (tokenizer->start != tokenizer->end)
+		tokenizer->end++;
+	skip_whitespace(tokenizer);
+	if (*tokenizer->end == '\0')
+	{
+		//종료
+	}
+	tokenizer->start = tokenizer->end;
 }
 
-t_bool	match(t_tokenizer *token, char matchword)
+void	skip_whitespace(t_tokenizer *tokenizer)
 {
-	if (*(token->current_token) != matchword)
-		return (FALSE);
-	if (*(token->current_token) != NULL)
-		token->current_token++;
-	else
-		token->token_size = 1;
-	return (TRUE);
+	while ((*tokenizer->end) == 32 || \
+		((*tokenizer->end) >= 9 && (*tokenizer->end) <= 13))
+		tokenizer->end++;
+	tokenizer->start = tokenizer->end;
 }
 
-t_bool	string_close(t_tokenizer *token, char c)
+t_bool	match(t_tokenizer *tokenizer, char matchword)
 {
-	token->current_token++;
-	while (*(token->current_token) != c && token->current_token)
-		token->current_token++;
-	if (*(token->current_token) != c || !token->current_token)
+	tokenizer->end++;
+	if (tokenizer->end && *(tokenizer->end) == matchword)
+		return (TRUE);
+	return (FALSE);
+}
+
+t_bool	string_close(t_tokenizer *tokenizer, char c)
+{
+	tokenizer->end++;
+	while (*tokenizer->end != '\0' && *tokenizer->end != c)
+		tokenizer->end++;
+	if (*tokenizer->end == '\0' || *tokenizer->end != c)
 		return (FALSE);
 	return (TRUE);
 }
@@ -38,15 +43,25 @@ t_bool	string_close(t_tokenizer *token, char c)
 #include <stdio.h>
 int main(void)
 {
-	char *line = "<<";
+	char *line = "'hihihihi''hello";
 	t_tokenizer token1;
-	t_tokenizer token2;
-	
-	line = skip_whitespace(line);
+	//t_tokenizer token2;
 
-	token1 = get_next_token(line);
-	printf("token_size %d\n", token1.token_size);
+	token1.start = line;
+	token1.end = line;
+	token1.type = E0F;
+
+	token1 = get_next_token(&token1);
+	printf("start ptr: %s\nended ptr: %s\n", token1.start, token1.end);
 	printf("%u\n", token1.type);
+
+	token1 = get_next_token(&token1);
+	printf("start ptr: %s\nended ptr: %s\n", token1.start, token1.end);
+	printf("%u\n", token1.type);
+
+	// token1 = get_next_token(&token1);
+	// printf("start ptr: %s\nended ptr: %s\n", token1.start, token1.end);
+	// printf("%u\n", token1.type);
 
 	// token2 = get_next_token(token1.current_token + token1.token_size);
 	// printf("token_size %d\n", token2.token_size);
