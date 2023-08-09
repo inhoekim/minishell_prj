2ff7a055afc90e2a8c34c0a4f8d3ed8f2d4e194a
seyeon22222 <kksy0917@gmail.com>
2023-08-09 15:12:58 +0900

    add builtin(exit_code)

--- unit_test/test1.c
+++ unit_test/test1.c
@@ -4,19 +4,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "../libft/libft.h"
 int main(int argc, char **argv, char **envp) 
 {
-  char path[PATH_MAX];
+//   char path[PATH_MAX];
   (void)argc;
   (void)argv;
+  (void)envp;
 
-  getcwd(path, PATH_MAX);
-//   printf("Current Directory: %s\n", path) ;
-  int idx = 0;
-  while (envp[idx])
-  {
-	printf("%s\n", envp[idx]);
-	idx++;
-  }
+//   getcwd(path, PATH_MAX);
+// //   printf("Current Directory: %s\n", path) ;
+//   int idx = 0;
+//   while (envp[idx])
+//   {
+// 	printf("%s\n", envp[idx]);
+// 	idx++;
+//   }
+	char *aaa = "temp";
+	// char	*temp = "123";
+	printf("%d\n", ft_atoi(aaa));
   return 0 ;
 }
