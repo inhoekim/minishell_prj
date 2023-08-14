9df35cde967755cbf173fc07fe0236feadbdfff3
seyeon22222 <92151066+seyeon22222@users.noreply.github.com>
2023-08-14 17:54:47 +0900

    Make wildcard

--- src/msh_utils.c
+++ src/msh_utils.c
@@ -2,7 +2,8 @@
 #include "../libft/libft.h"
 #include "../include/minishell.h"
 # define STDERR_FD 2
-
+# define PATH_MAX 1024
+#include <dirent.h>
 
 void	syntax_error(t_tokenizer *tokenizer)
 {
@@ -17,19 +18,85 @@ void	syntax_error(t_tokenizer *tokenizer)
 	set_exit_status(STDERR_FD);
 }
 
-void	exec_error(t_context *p_ctx)
+t_list	*pathname_expansion(t_list *list, t_bool glob_flag)
 {
-	int status;
-	
-	status = set_exit_status(p_ctx->exit_status);
-	if (status == 127)
-		ft_putstr_fd("minishell : Command not found ", STDERR_FD);
-	else if (status == 126)
-		ft_putstr_fd("minishell : Permision Denied ", STDERR_FD);
-	else
+	t_list		  	*temp_list;
+	char			path[PATH_MAX];
+	char			*dir;
+	char			*pattern;
+
+	pattern = make_pattern(list);
+	dir = getcwd(path, PATH_MAX);
+	if (glob_flag)
 	{
-		status += 128;
-		set_exit_status(status);
-		p_ctx->exit_status = status;
+		temp_list = make_templist(dir, pattern);
+		free(pattern);
+		return (temp_list);
 	}
+	return (ft_lstnew(pattern));
+}
+
+char	*make_pattern(t_list *list)
+{
+	char	*newstr;
+	char	*aux;
+	int		len;
+	t_list	*temp;
+
+	len = ft_lstsize(list);
+	newstr = calloc(sizeof(*newstr), len + 1);
+	temp = list;
+	while (list)
+	{
+		aux = list->content;
+		ft_strlcat(newstr, aux, len + 1);
+		list = list->next;
+	}
+	return (newstr);
+}
+
+t_list  *make_templist(char *dir, char *pattern)
+{
+	DIR				*dir_ptr;
+	struct dirent	*file;
+	t_list			**temp;
+
+	temp = NULL;
+	dir_ptr = opendir(dir);
+	file = readdir(dir_ptr);
+	while (file)
+	{
+		if (file && file->d_type == DT_REG && wildcard(file->d_name, pattern))
+			ft_lstadd_back(temp, ft_lstnew(ft_strdup(file->d_name)));
+		file = readdir(dir_ptr);
+	}
+	closedir(dir_ptr);
+	return (*temp);
+}
+
+t_bool	*wildcard(char *name, char *pattern)
+{
+	int len_n;
+	int	len_p;
+	int	idx;
+	int	flag;
+
+	len_n = ft_strlen(name);	
+	len_p = ft_strlen(pattern);
+	idx = 0;
+	while (idx < len_n && idx < len_p && (pattern[idx] == "?" || pattern[idx] == name[idx]))
+		idx++;
+	if (len_p == idx)
+		return (TRUE);
+	if (pattern[idx] == "*")
+	{
+		flag = 0;
+		while (flag + idx <= len_n)
+		{
+			if (wildcard(name + idx + 1, pattern + flag + idx))
+				return (TRUE);
+			flag++;
+		}
+	}
+	return (FALSE);
 }
