9bec8c15809e71772a7768e39a99ab5557e9a926
Inhoe Kim <inhoe.kim95@gmail.com>
2023-08-15 18:23:23 +0900

    change: parser.c (프리관련 작업진행)

--- parser/parser.c
+++ parser/parser.c
@@ -6,16 +6,17 @@
 /*   By: inhkim <inhkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:25:54 by naylee            #+#    #+#             */
-/*   Updated: 2023/08/15 17:45:35 by inhkim           ###   ########.fr       */
+/*   Updated: 2023/08/15 18:18:34 by inhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/rule.h"
 #include "../include/tokenizer.h"
+#define STDERR_FD 2
 
 t_node	*parser(char *line)
- {
+{
 	t_node		*root;
 	t_tokenizer	tokenizer;
 
@@ -25,17 +26,26 @@ t_node	*parser(char *line)
 	root = msh_grammar(&tokenizer);
 	if (get_curr_token(&tokenizer)->type != E0F)
 	{
-		syntax_error("error");
-		//free_tree(root);
+		syntax_error(&tokenizer);
+		free_tree(root);
+		root = NULL;
 	}
 	free(tokenizer.curr_token->str);
 	free(tokenizer.curr_token);
 	return (root);
- }
+}
 
-void	syntax_error(char * str)
+void	syntax_error(t_tokenizer *tokenizer)
 {
-	return ;
+	t_token	*token;
+
+	token = tokenizer->curr_token;
+	ft_putstr_fd("minishell : ", STDERR_FD);
+	ft_putstr_fd("syntax error near unexpected token ", STDERR_FD);
+	ft_putchar_fd('\'', STDERR_FD);
+	ft_putstr_fd(token->str, STDERR_FD);
+	ft_putchar_fd('\'', STDERR_FD);
+	ft_putchar_fd("\n", STDERR_FD);
 }
 
 void	free_tree(t_node *root)
@@ -45,7 +55,7 @@ void	free_tree(t_node *root)
 
 int	main(int argc, char **argv)
 {
-	t_node *test;
+	t_node	*test;
 	test = parser("ls | ls >a | \"l\"s");
 	int a = 3;
 	a++;
