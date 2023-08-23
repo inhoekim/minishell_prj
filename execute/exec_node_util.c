40efc87af676bf4f21b897caf24f63e26f0966f7
sdg <eorms6199@gmail.com>
2023-08-23 10:33:49 +0900

    merge: inhkim

--- execute/exec_node_util.c
+++ execute/exec_node_util.c
@@ -29,7 +29,7 @@ void	exec_subshell(t_node *node, t_context *p_ctx)
 	if (p_ctx->fd[STDOUT] != STDOUT)
 		close(p_ctx->fd[STDOUT]);
 	enqueue(pid, p_ctx);
-	//wait_queue(p_ctx);
+	wait_queue(p_ctx);
 }
 
 void	exec_or(t_node *node, t_context *p_ctx)
