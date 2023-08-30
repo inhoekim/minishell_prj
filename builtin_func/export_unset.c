355fd379238ee4322b9d00fb0a1f322ec6fd5865
Daegeun Song <eorms6199@gmail.com>
2023-08-30 13:14:26 +0900

    edit: ready to header

--- builtin_func/export_unset.c
+++ builtin_func/export_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_unset.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdg <sdg@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: dasong <dasong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:42:49 by seykim            #+#    #+#             */
-/*   Updated: 2023/08/29 19:58:26 by sdg              ###   ########.fr       */
+/*   Updated: 2023/08/30 13:14:09 by dasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ t_bool	ft_export(char **argv)
 	}
 	else
 	{
-		env = get_envp();
 		while (argv[++idx])
 		{
 			if (check_argv(argv[idx]))
@@ -84,7 +83,6 @@ void	check_env(char **argv, t_list **env)
 		}
 		free(temp);
 	}
-	
 }
 
 char	*make_temp(char *s1)
