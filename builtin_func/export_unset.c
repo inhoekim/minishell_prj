1d889729eec3956754ef2cfc17c8a3f2e62af9c2
seyeon22222 <kksy0917@gmail.com>
2023-08-30 16:46:10 +0900

    wait_process && builtin

--- builtin_func/export_unset.c
+++ builtin_func/export_unset.c
@@ -6,7 +6,7 @@
 /*   By: seykim <seykim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:42:49 by seykim            #+#    #+#             */
-/*   Updated: 2023/08/30 16:33:48 by seykim           ###   ########.fr       */
+/*   Updated: 2023/08/30 16:44:40 by seykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ t_bool	ft_export(char **argv)
 	}
 	else
 	{
-		env = get_envp();
 		while (argv[++idx])
 		{
 			if (check_argv(argv[idx]))
@@ -84,7 +83,6 @@ void	check_env(char **argv, t_list **env)
 		}
 		free(temp);
 	}
-	
 }
 
 char	*make_temp(char *s1)
