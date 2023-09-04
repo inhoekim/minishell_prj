50c838929617099798470239d951e35323aaa98b
seyeon22222 <kksy0917@gmail.com>
2023-09-04 20:01:14 +0900

    change cd -> unset PWD, OLDPWD => print

--- Makefile
+++ Makefile
@@ -31,7 +31,7 @@ OBJS = $(SRCS:.c=.o)
 ## compile
 CC=	gcc
 CFLAGS = -Wall -Wextra -Werror $(addprefix -I,$(INC_DIRS))
-LDFLAGS= $(addprefix -L,$(LIB_DIRS)) -lreadline -lft -fsanitize=address
+LDFLAGS= $(addprefix -L,$(LIB_DIRS)) -lreadline -lft
 
 .PHONY: clean, fclean, re, all
 
--- builtin_func/builtin_cd.c
+++ builtin_func/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: seykim <seykim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:11:03 by seykim            #+#    #+#             */
-/*   Updated: 2023/09/04 19:27:41 by seykim           ###   ########.fr       */
+/*   Updated: 2023/09/04 19:59:38 by seykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,33 @@ static void	cd_util(char *newpwd)
 
 	if (newpwd)
 		set_envp("OLDPWD", newpwd);
+	// unset으로 PWD, OLDPWD를 지웠을 경우
+	// PWD= / OLDPWD= 를 출력하는 부분
 	else
 	{
 		newpwd = ft_getenv("PWD");
 		temp = ft_getenv("OLDPWD");
-		set_envp("OLDPWD", newpwd);
-		set_envp("PWD", temp);
+		if (!newpwd)
+			set_envp("OLDPWD", "\"\"");
+		else
+			set_envp("OLDPWD", newpwd);
+		if (!temp)
+			set_envp("PWD", "\"\"");
+		else
+			set_envp("PWD", temp);
 		free(temp);
 		free(newpwd);
 	}
+	// 안지우고 경로 업데이트하는 방식
+	// else
+	// {
+	// 	newpwd = ft_getenv("PWD");
+	// 	temp = ft_getenv("OLDPWD");
+	// 	set_envp("OLDPWD", newpwd);
+	// 	set_envp("PWD", temp);
+	// 	free(temp);
+	// 	free(newpwd);
+	// }
 	newpwd = getcwd(path, PATH_MAX);
 	if (newpwd)
 		set_envp("PWD", newpwd);
